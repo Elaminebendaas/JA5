3b317ac4b0d3e9d7b6476c4f7865e4dda8a5e4c4
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-04-12 10:39:28 -0400

    Initial Commit

---
+++ MyBlobs.java
@@ -0,0 +1,36 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+/* -----------------------------------------
+   Note: The ArrayDeque is an implementation 
+         of the Deque ADT. That is, it is a 
+		   double-ended queue. 
+
+		   You can simulate both a Stack and 
+		   a regular Queue with this data structure
+		   in the following way:
+
+		 Stack: push  ~ addFirst
+		        pop   ~ removeFirst
+		
+		 Queue: enqueue ~ addLast
+		        dequeue ~ removeFirst
+  ------------------------------------------ */
+
+public class MyBlobs extends Blobs{
+
+
+	///////////////////////////////////////////////////////////////////////	
+	///////////////////////////////////////////////////////////////////////	
+	// do NOT change or remove this constructor. We will use it to create 
+	// objects when testing your code. If it is removed, we cannot test 
+	// your code!
+	///////////////////////////////////////////////////////////////////////	
+	public MyBlobs(){}
+	///////////////////////////////////////////////////////////////////////	
+	///////////////////////////////////////////////////////////////////////	
+
+
+	
+	
+}
